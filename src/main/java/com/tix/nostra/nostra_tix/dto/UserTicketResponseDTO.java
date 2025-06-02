567810f0ae4c46e7e1933f96815e8dcce9bf1b65
melankolia <setyo.nugroho96@gmail.com>
2025-06-02 20:53:52 +0700

    feat: Create GET: /api/user-tickets/{userId}

---
+++ src/main/java/com/tix/nostra/nostra_tix/dto/UserTicketResponseDTO.java
@@ -0,0 +1,12 @@
+package com.tix.nostra.nostra_tix.dto;
+
+import java.util.List;
+
+public record UserTicketResponseDTO(
+        Long id,
+        String movieName,
+        String movieImageURI,
+        String theaterName,
+        String studioName,
+        List<String> seatNumbers) {
+}
--- src/main/java/com/tix/nostra/nostra_tix/service/BookingService.java
+++ src/main/java/com/tix/nostra/nostra_tix/service/BookingService.java
@@ -1,11 +1,16 @@
 package com.tix.nostra.nostra_tix.service;
 
+import java.util.List;
+
 import com.tix.nostra.nostra_tix.dto.BookingDTO;
 import com.tix.nostra.nostra_tix.dto.BookingSeatResponseDTO;
+import com.tix.nostra.nostra_tix.dto.UserTicketResponseDTO;
 
 public interface BookingService {
 
     BookingSeatResponseDTO findAll(Long scheduleId, Long studioId);
+
+    List<UserTicketResponseDTO> findByUserId(Long userId);
 
     Boolean createBooking(Long scheduleId, BookingDTO bookingDTO);
 
