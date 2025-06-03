9ad03cc0a79be1fdc188f556ed83a2a0ea7276d5
melankolia <setyo.nugroho96@gmail.com>
2025-06-03 16:03:22 +0700

    feat: Create Service CUD for Domain Movie

--- src/main/java/com/tix/nostra/nostra_tix/service/Impl/MovieServiceImpl.java
+++ src/main/java/com/tix/nostra/nostra_tix/service/Impl/MovieServiceImpl.java
@@ -1,5 +1,6 @@
 package com.tix.nostra.nostra_tix.service.Impl;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.tix.nostra.nostra_tix.domain.Movie;
+import com.tix.nostra.nostra_tix.dto.MovieRequestDTO;
 import com.tix.nostra.nostra_tix.exception.ResourceNotFoundException;
 import com.tix.nostra.nostra_tix.repository.MovieRepository;
 import com.tix.nostra.nostra_tix.service.FileService;
@@ -33,13 +35,42 @@ public class MovieServiceImpl implements MovieService {
 
     @Override
     public Movie findById(Long id) {
-        return movieRepository.findById(id).orElse(null);
+        return movieRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Movie not found with id: " + id));
+    }
+
+    @Override
+    public Movie createMovie(MovieRequestDTO movieRequestDTO) {
+        Movie movie = new Movie();
+        movie.setName(movieRequestDTO.name());
+        movie.setTrailerURI(movieRequestDTO.trailerURI());
+        movie.setImageURI(movieRequestDTO.imageURI());
+        movie.setDuration(movieRequestDTO.duration());
+        movie.setSynopsis(movieRequestDTO.synopsis());
+        movie.setShowingStartDate(new Date(movieRequestDTO.showingStartDate() * 1000));
+        movie.setShowingEndDate(new Date(movieRequestDTO.showingEndDate() * 1000));
+
+        return movieRepository.save(movie);
+    }
+
+    @Override
+    public Movie updateMovie(Long id, MovieRequestDTO movieRequestDTO) {
+        Movie existingMovie = findById(id);
+
+        existingMovie.setName(movieRequestDTO.name());
+        existingMovie.setTrailerURI(movieRequestDTO.trailerURI());
+        existingMovie.setImageURI(movieRequestDTO.imageURI());
+        existingMovie.setDuration(movieRequestDTO.duration());
+        existingMovie.setSynopsis(movieRequestDTO.synopsis());
+        existingMovie.setShowingStartDate(new Date(movieRequestDTO.showingStartDate() * 1000));
+        existingMovie.setShowingEndDate(new Date(movieRequestDTO.showingEndDate() * 1000));
+
+        return movieRepository.save(existingMovie);
     }
 
     @Override
     public Movie updateMovie(Long movieId, MultipartFile file) {
-        Movie existingMovie = movieRepository.findById(movieId)
-                .orElseThrow(() -> new ResourceNotFoundException("Movie not found"));
+        Movie existingMovie = findById(movieId);
 
         try {
             String filePath = fileService.uploadFile(file);
@@ -53,10 +84,14 @@ public class MovieServiceImpl implements MovieService {
 
     @Override
     public Movie updateMovie(Long movieId, String imageURI) {
-        Movie existingMovie = movieRepository.findById(movieId)
-                .orElseThrow(() -> new ResourceNotFoundException("Movie not found"));
-
+        Movie existingMovie = findById(movieId);
         existingMovie.setImageURI(imageURI);
         return movieRepository.save(existingMovie);
+    }
+
+    @Override
+    public void deleteMovie(Long id) {
+        Movie movie = findById(id);
+        movieRepository.delete(movie);
     }
 }
--- src/main/java/com/tix/nostra/nostra_tix/service/MovieService.java
+++ src/main/java/com/tix/nostra/nostra_tix/service/MovieService.java
@@ -5,16 +5,23 @@ import java.util.List;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.tix.nostra.nostra_tix.domain.Movie;
+import com.tix.nostra.nostra_tix.dto.MovieRequestDTO;
 
 public interface MovieService {
 
     List<Movie> findAll();
 
-    Movie findById(Long number);
+    Movie findById(Long id);
 
     List<Movie> findAllUpcoming();
+
+    Movie createMovie(MovieRequestDTO movieRequestDTO);
+
+    Movie updateMovie(Long id, MovieRequestDTO movieRequestDTO);
 
     Movie updateMovie(Long movieId, MultipartFile file);
 
     Movie updateMovie(Long movieId, String imageURI);
+
+    void deleteMovie(Long id);
 }
