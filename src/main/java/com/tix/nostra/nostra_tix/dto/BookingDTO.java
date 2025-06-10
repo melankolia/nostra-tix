b2be6d61871781580832cdf6ff699dab223e247c
melankolia <setyo.nugroho96@gmail.com>
2025-06-10 10:49:30 +0700

    feat: Buat validasi pengecekan Kursi Mati di POST:/api/booking/{scheduleId}

--- src/main/java/com/tix/nostra/nostra_tix/dto/BookingDTO.java
+++ src/main/java/com/tix/nostra/nostra_tix/dto/BookingDTO.java
@@ -1,12 +1,12 @@
 package com.tix.nostra.nostra_tix.dto;
 
-import java.util.Set;
+import java.util.List;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record BookingDTO(
-        Long scheduleId,
-        @NotNull(message = "User ID is required") Long userId,
-        @NotEmpty(message = "At least one seat must be selected") Set<Long> seatIds) {
+                Long scheduleId,
+                @NotNull(message = "User ID is required") Long userId,
+                @NotEmpty(message = "At least one seat must be selected") List<Long> seatIds) {
 }
--- src/main/java/com/tix/nostra/nostra_tix/repository/SeatRepository.java
+++ src/main/java/com/tix/nostra/nostra_tix/repository/SeatRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.tix.nostra.nostra_tix.domain.Seat;
 import com.tix.nostra.nostra_tix.projection.SeatByStudioIdProjection;
@@ -25,5 +26,21 @@ public interface SeatRepository extends JpaRepository<Seat, Long> {
                 WHERE s.studio.id = :studioId
             """)
     List<SeatByStudioIdProjection> findByStudioIdProjectedBy(Long studioId);
+
+    @Query("""
+                SELECT s.id as id,
+                    s.seatNumber as seatNumber,
+                    s.rowIndex as rowIndex,
+                    s.columnIndex as columnIndex,
+                    s.seatType.name as seatTypeName,
+                    s.seatType.additionalPrice as additionalPrice,
+                    s.isAvailable as available,
+                    s.isVisible as visible
+                FROM Seat s
+                WHERE s.studio.id = :studioId AND s.rowIndex = :rowIndex
+                ORDER BY s.columnIndex ASC
+            """)
+    List<SeatByStudioIdProjection> findByStudioIdAndRowIndexProjectedBy(@Param("studioId") Long studioId,
+            @Param("rowIndex") Integer rowIndex);
 
 }
